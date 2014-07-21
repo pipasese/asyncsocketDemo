a41022422fc683c4cb5e9492cec84ef023dc3667
vic <vic@vicdemac-mini.lan>
2014-07-21 10:32:52 +0800

    Initial Commit

---
+++ asyncsocketDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  asyncsocketDemo
+//
+//  Created by vic on 14-7-21.
+//  Copyright (c) 2014年 vic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
